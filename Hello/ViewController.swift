7c15d114a4391739c87f90750aa1ec11abad7ab9
SDS-103 <sds-103@70-12-216-105.pools.spcsdns.net>
2016-04-25 16:21:50 +0900

    프로젝트 시작.

---
+++ Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by SDS-103 on 2016. 4. 25..
+//  Copyright © 2016년 SDS-103. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
